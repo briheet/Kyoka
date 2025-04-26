62cfc93f34cd350cfcd69cff4155cb68eb16e125
briheet <briheetyadav@gmail.com>
2025-04-26 16:16:38 +0530

    add: logger

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,63 @@
+use std::env;
+
+use dotenv::dotenv;
 use eframe::egui;
 
 fn main() {
+    // Load env for data n ol
+    dotenv().ok();
+
+    // Setup logging
+    if std::env::var_os("RUST_LOG").is_none() {
+        unsafe {
+            std::env::set_var("RUST_LOG", "info");
+        }
+    }
+
+    pretty_env_logger::init();
+
+    log::info!("started the porject");
+
+    // Setup gui
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(
         "Kyoka",
         native_options,
         Box::new(|cc| Ok(Box::new(MyEguiApp::new(cc)))),
-    );
+    )
+    .ok();
+
+    let name = env::var("NAME");
+
+    match name {
+        Ok(val) => println!("name: {}", val),
+        Err(e) => println!("err: {}", e),
+    }
+
+    log::info!("exiting...");
 }
 
 #[derive(Default)]
 struct MyEguiApp {}
 
 impl MyEguiApp {
-    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
         Self::default()
     }
 }
 
 impl eframe::App for MyEguiApp {
-    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::TopBottomPanel::top("top panel").show(ctx, |ui| {
+            ui.label("Top Panel");
+        });
+
+        egui::SidePanel::left("my_left_panel").show(ctx, |ui| {
+            ui.label("Left panel");
+        });
+
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Hello");
+            ui.heading("gogo gaga");
         });
     }
 }
